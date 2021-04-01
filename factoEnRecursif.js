9e3a290fd7b1c2825fb6fe339424eecf5f970ac0
kardev38 <moujahedk83@gmail.com>
2021-04-01 18:47:56 +0200

    ne fonction pas

---
+++ factoEnRecursif.js
@@ -0,0 +1,25 @@
+// Créer un script qui calcul la factoriel du nombre passé en argument.
+// Ex. d’appel du script :
+// $> ./factoriel 3
+// 6
+
+
+
+var readline = require("readline-sync");
+var factorielle = readline.questionInt("Quelle factorielle voulez-vous : ");
+var resultat = factorielle ;
+
+for(var i = 1 ; i <= factorielle ; i++){
+    if (n<=1) {
+        return 1
+    } else 
+            {return n*resultat(n-1)}
+    }
+      
+    
+  
+
+
+console.log(resultat);
+
+
